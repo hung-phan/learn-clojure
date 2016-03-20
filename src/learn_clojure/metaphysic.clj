bf7536b44b4cc27c519e62e80ee39830b8fda986
hung-phan <colorvisavn@gmail.com>
2016-03-20 21:08:47 +0700

    Update learn-clojure

--- src/learn_clojure/metaphysic.clj
+++ src/learn_clojure/metaphysic.clj
@@ -103,6 +103,15 @@
 (pprint
  (:socks @dryer))
 
+
+;; commute allows you to update a ref’s state within a transaction, just like alter.
+;; However, its behavior at commit time is completely different. Here’s how alter behaves:
+
+;; Reach outside the transaction and read the ref’s current state.
+;; Compare the current state to the state the ref started with within the transaction.
+;; If the two differ, make the transaction retry.
+;; Otherwise, commit the altered ref state.
+
 (def counter (ref 0))
 
 (future
@@ -117,13 +126,6 @@
 
 (println @counter)
 
-;; commute allows you to update a ref’s state within a transaction, just like alter.
-;; However, its behavior at commit time is completely different. Here’s how alter behaves:
-
-;; Reach outside the transaction and read the ref’s current state.
-;; Compare the current state to the state the ref started with within the transaction.
-;; If the two differ, make the transaction retry.
-;; Otherwise, commit the altered ref state.
 ;; commute, on the other hand, behaves like this at commit time:
 
 ;; Reach outside the transaction and read the ref’s current state.
@@ -141,3 +143,95 @@
 
 (future (dosync (commute counter (sleep-print-update 100 "Thread A" inc))))
 (future (dosync (commute counter (sleep-print-update 150 "Thread B" inc))))
+
+(def receiver-a (ref #{}))
+(def receiver-b (ref #{}))
+(def giver (ref #{1}))
+(do (future (dosync (let [gift (first @giver)]
+                      (Thread/sleep 10)
+                      (commute receiver-a conj gift)
+                      (commute giver disj gift))))
+    (future (dosync (let [gift (first @giver)]
+                      (Thread/sleep 50)
+                      (commute receiver-b conj gift)
+                      (commute giver disj gift)))))
+
+@receiver-a
+@receiver-b
+@giver
+
+;; var
+
+(def ^:dynamic *notification-address* "dobby@elf.org")
+
+*notification-address*
+
+(binding [*notification-address* "test@elf.org"]
+  *notification-address*)
+
+(binding [*notification-address* "tester-1@elf.org"]
+  (println *notification-address*)
+  (binding [*notification-address* "tester-2@elf.org"]
+    (println *notification-address*))
+  (println *notification-address*))
+
+(binding [*out* (clojure.java.io/writer "print-output")]
+  (prn *out*)
+  (println "A man who carries a cat by the tail learns
+something he can learn in no other way.
+-- Mark Twain"))
+
+(slurp "print-output")
+
+(def ^:dynamic *troll-thought* nil)
+
+(defn troll-riddle
+  [your-answer]
+  (let [number "man meat"]
+    (when (thread-bound? #'*troll-thought*)
+      (set! *troll-thought* number))
+    (if (= number your-answer)
+      "TROLL: You can cross the bridge!"
+      "TROLL: Time to eat you, succulent human!")))
+
+(binding [*troll-thought* nil]
+  (println (troll-riddle 2))
+  (println "SUCCULENT HUMAN: Oooooh! The answer was" *troll-thought*))
+
+;; binding
+(let [out *out*]
+  (.start
+   (Thread. #(binding [*out* out]
+               (.write *out* "prints to repl from thread")))))
+
+(.start (Thread. (bound-fn [] (.write *out* "prints to repl from thread"))))
+
+;; alter-var-root
+(def power-source "hair")
+
+(alter-var-root #'power-source (fn [_] "7-eleven parking lot"))
+
+power-source
+
+(with-redefs [*out* *out*]
+  (doto (Thread. #(println "with redefs allows me to show up in the REPL"))
+    .start
+    .join))
+
+(def alphabet-length 26)
+
+;; Vector of chars, A-Z
+(def letters (mapv (comp str char (partial + 65)) (range alphabet-length)))
+
+(defn random-string
+  "Returns a random string of specified length"
+  [length]
+  (apply str (take length (repeatedly #(rand-nth letters)))))
+
+(defn random-string-list
+  [list-length string-length]
+  (doall (take list-length (repeatedly (partial random-string string-length)))))
+
+(def orc-names (random-string-list 3000 7000))
+
+(time (dorun (pmap clojure.string/lower-case orc-names)))
